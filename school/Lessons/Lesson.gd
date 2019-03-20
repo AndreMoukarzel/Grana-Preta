b219419391662ce675174b1e1e25aa97da99e40a
Detril <andremoukarzel@gmail.com>
2019-03-20 14:02:54 -0300

    Add basic content placer

--- school/Lessons/Lesson.gd
+++ school/Lessons/Lesson.gd
@@ -1,10 +1,33 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-const OFFSET = Vector2(10, 5)
 
-# Called when the node enters the scene tree for the first time.
+const OFFSET = Vector2(10, 5)
+const TEXT_OFFSET = 10
+
+
 func _ready():
 	rect_size = OS.get_window_size() - OFFSET
 	rect_position = OFFSET/2
+	add_content(["blabla", "Image.png", "Mingau"])
+
+func add_content(content):
+	var hpos = 10
+	
+	for c in content:
+		if c.find(".png") != -1: # is an image
+			var texture = load(str("res://school/Lessons/", c))
+			var tr = TextureRect.new()
+			var tex_size = texture.get_size()
+			tr.texture = texture
+			tr.rect_position = Vector2((OS.get_window_size().x - tex_size.x)/2, hpos)
+			add_child(tr)
+			
+			hpos += tex_size.y + 5
+		else:
+			var l = Label.new()
+			l.add_color_override("font_color", Color(0, 0, 0))
+			l.rect_position = Vector2(TEXT_OFFSET, hpos)
+			l.text = c
+			add_child(l)
+			
+			hpos += l.rect_size.y + 5
