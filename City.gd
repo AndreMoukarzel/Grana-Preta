f258f5da2d9f05b950f205bfc410dfd04c37991d
Andre Ferrari Moukarzel <Detril@users.noreply.github.com>
2019-04-08 19:02:30 -0300

    Base Broker

--- City.gd
+++ City.gd
@@ -14,6 +14,13 @@ func _on_School_pressed():
 
 func _on_House_pressed():
 	print("Enter House")
-	var e = get_tree().change_scene("res://House.tscn")
+	var e = get_tree().change_scene("res://house/House.tscn")
 	if e != 0:
 		print("House scene couldn't be loaded")
+
+
+func _on_Broker_pressed():
+	print("Enter Broker")
+	var e = get_tree().change_scene("res://broker/Broker.tscn")
+	if e != 0:
+		print("Broker scene couldn't be loaded")
