06cd4d87bc240ef5f1c8090b1591fe86bdc90a43
Detril <andremoukarzel@gmail.com>
2019-04-15 20:15:53 -0300

    Fix BondDisplay expanding incorrecly

--- broker/BondDisplay.gd
+++ broker/BondDisplay.gd
@@ -1,16 +1,19 @@
 extends Control
 
 const BOND_SCN = preload("res://broker/bond/Bond.tscn")
-const BOND_EXPANSION = 380
+const BOND_EXPANSION = 300
 const TWN_TIME = .2
 
 onready var SafeBonds = $Safe/Bonds
 onready var ModerBonds = $Moderate/Bonds
 onready var ChanceBonds = $Chanceful/Bonds
+onready var s_pos = $Safe.rect_position.y
+onready var m_pos = $Moderate.rect_position.y
+onready var c_pos = $Chanceful.rect_position.y
 
 
 func _ready():
-	for i in range(3):
+	for i in range(2):
 		var Bond = BOND_SCN.instance()
 		Bond.rect_position.y = i * 100
 		SafeBonds.add_child(Bond)
@@ -25,16 +28,19 @@ func close_all_bonds(exception = null):
 			continue
 		if child.is_open:
 			child.close()
+			bond_closed(child)
 	for child in $Moderate/Bonds.get_children():
 		if exception and child == exception:
 			continue
 		if child.is_open:
 			child.close()
+			bond_closed(child)
 	for child in $Chanceful/Bonds.get_children():
 		if exception and child == exception:
 			continue
 		if child.is_open:
 			child.close()
+			bond_closed(child)
 
 
 func disable_all_bonds(disable = true):
@@ -54,15 +60,13 @@ func bond_opened(bond):
 	close_all_bonds(bond)
 	for child in bond.get_parent().get_children():
 		if child.rect_global_position.y > pos_y:
-			var c_pos = child.rect_position.y
-			$Tween.interpolate_property(child, "rect_position:y", c_pos, c_pos + BOND_EXPANSION, TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+			var pos = child.rect_position.y
+			$Tween.interpolate_property(child, "rect_position:y", pos, pos + BOND_EXPANSION, TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	
 	if group != "Chanceful":
-		var g_pos = $Chanceful.rect_position.y
-		$Tween.interpolate_property($Chanceful, "rect_position:y", g_pos, g_pos + BOND_EXPANSION, TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+		$Tween.interpolate_property($Chanceful, "rect_position:y", null, c_pos + BOND_EXPANSION, TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	if group == "Safe":
-		var g_pos = $Moderate.rect_position.y
-		$Tween.interpolate_property($Moderate, "rect_position:y", g_pos, g_pos + BOND_EXPANSION, TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+		$Tween.interpolate_property($Moderate, "rect_position:y", null, m_pos + BOND_EXPANSION, TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.start()
 	yield($Tween, "tween_completed")
 	disable_all_bonds(false)
@@ -75,15 +79,13 @@ func bond_closed(bond):
 	disable_all_bonds()
 	for child in bond.get_parent().get_children():
 		if child.rect_global_position.y > pos_y:
-			var c_pos = child.rect_position.y
-			$Tween.interpolate_property(child, "rect_position:y", c_pos, c_pos - BOND_EXPANSION, TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+			var pos = child.rect_position.y
+			$Tween.interpolate_property(child, "rect_position:y", pos, pos - BOND_EXPANSION, TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	
 	if group != "Chanceful":
-		var g_pos = $Chanceful.rect_position.y
-		$Tween.interpolate_property($Chanceful, "rect_position:y", g_pos, g_pos - BOND_EXPANSION, TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+		$Tween.interpolate_property($Chanceful, "rect_position:y", null, c_pos, TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	if group == "Safe":
-		var g_pos = $Moderate.rect_position.y
-		$Tween.interpolate_property($Moderate, "rect_position:y", g_pos, g_pos - BOND_EXPANSION, TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+		$Tween.interpolate_property($Moderate, "rect_position:y", null, m_pos, TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.start()
 	yield($Tween, "tween_completed")
 	disable_all_bonds(false)
