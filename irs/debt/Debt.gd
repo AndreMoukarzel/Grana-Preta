c94f636374f7a02f6a0fe2717d7030c7a4539590
Andre Ferrari Moukarzel <Detril@users.noreply.github.com>
2019-08-07 10:07:14 -0300

    Add strikes info

--- irs/debt/Debt.gd
+++ irs/debt/Debt.gd
@@ -23,7 +23,7 @@ func setup(source_name : String, buy_date, sell_date, ammount_sold : int, profit
 
 	$SourceName.text = source_name
 	self.strikes = max(0, Save.get_time_difference(sell_date, OS.get_date())[0])
-	# Strike display
+	$Strikes/Sprite.frame = min(strikes, 0) 
 	self.value = calculate_value(profit, taxes)
 	$Value.text = str("G$", self.value)
 	self.buy_date = buy_date
@@ -61,6 +61,7 @@ func close():
 	$Tween.interpolate_property($Value, 'rect_position', null, Vector2(5, 55), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($BuyDate, 'rect_position', null, Vector2(50, 5), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($SellDate, 'rect_position', null, Vector2(50, 55), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	$Tween.interpolate_property($Strikes, 'modulate', null, Color(1, 1, 1, 0), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($AmmountSold, 'modulate', null, Color(1, 1, 1, 0), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($Profit, 'modulate', null, Color(1, 1, 1, 0), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($Taxes, 'modulate', null, Color(1, 1, 1, 0), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
@@ -81,6 +82,7 @@ func open():
 	$Tween.interpolate_property($Value, 'rect_position', null, Vector2(5, 540), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($BuyDate, 'rect_position', null, Vector2(-30, 55), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($SellDate, 'rect_position', null, Vector2(-30, 105), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	$Tween.interpolate_property($Strikes, 'modulate', null, Color(1, 1, 1, 1), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($AmmountSold, 'modulate', null, Color(1, 1, 1, 1), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($Profit, 'modulate', null, Color(1, 1, 1, 1), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($Taxes, 'modulate', null, Color(1, 1, 1, 1), TWN_TIME, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
