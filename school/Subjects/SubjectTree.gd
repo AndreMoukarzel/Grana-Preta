c25a2b5d514eb2d0b505dc1a6fbf8525092298a9
Detril <andremoukarzel@gmail.com>
2019-08-13 10:59:56 -0300

    Change presentation of SubjectTree
    Fix #60

--- school/Subjects/SubjectTree.gd
+++ school/Subjects/SubjectTree.gd
@@ -39,6 +39,7 @@ func position_subjects(theme_name):
 		var N = Node2D.new() # Node is used to alter Subjects' z-index
 		
 		N.set_position(Vector2(size/2, height))
+		N.set_name(str(get_child_count() - 2))
 		add_child(N)
 		N.add_child(Subject)
 		Subject.setup(size, subject[0], subject[1], subject[2])
@@ -47,21 +48,42 @@ func position_subjects(theme_name):
 
 func connect_signals():
 	for child in get_children():
-		if child.get_name() != "CanvasLayer":
+		if child.get_name() != "CanvasLayer" and child.get_name() != "Tween":
 			var Subject = child.get_node("Subject")
 			Subject.connect("opened", self, "subject_opened")
 			Subject.connect("closed", self, "subject_closed")
 
 
 func subject_opened(node):
+	var will_tween = false
+	var opening_height = (node.get_node("Subject").BASE_HEIGHT + node.get_node("Subject").lessons_height + 10) * 1.2
+	
 	for child in get_children():
-		if child != node and child.get_name() != "CanvasLayer":
+		if child != node and child.get_name() != "CanvasLayer" and child.get_name() != "Tween":
 			var Subject = child.get_node("Subject")
 			if Subject.is_open:
 				Subject.close()
 			child.z_index = 0
+			
+			if child.global_position.y > node.global_position.y:
+				var pos = int(child.get_name()) * HEIGHT_ADD + 80
+				will_tween = true
+				$Tween.interpolate_property(child, "position:y", null, pos + opening_height, .2, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	if will_tween:
+		$Tween.start()
 	node.z_index = 1
 
 
 func subject_closed(node):
+	var will_tween = false
 	node.z_index = 0
+	
+	for child in get_children():
+		if child != node and child.get_name() != "CanvasLayer" and child.get_name() != "Tween":
+			if child.global_position.y > node.global_position.y:
+				var pos = int(child.get_name()) * HEIGHT_ADD + 80
+				
+				will_tween = true
+				$Tween.interpolate_property(child, "position:y", null, pos, .2, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	if will_tween:
+		$Tween.start()
