e701e67e6e74be03cbda299fdfd864f847f66f5a
Detril <andremoukarzel@gmail.com>
2019-08-15 08:10:08 -0300

    Add central value line

--- IndexDisplay.gd
+++ IndexDisplay.gd
@@ -1,20 +1,18 @@
 extends Control
 
-var values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-var v_scale = 20
-var h_scale = 10
+var values = [0, -1, 2, 3, 11,-5, 6, -7, 8, -9, 10]
 
 class LineGraph:
 	var values = [0.0] # Values to be plotted in the graph
 	var pos = Vector2() # Where the graph will be positioned
 	var central_val : float # Value that will be in the midpoint of the graph
 	var val_range : float # Distance between top/bottom value and central value
-	var total_widht : int # total widht of the graph
+	var total_width : int # total width of the graph
 	var total_height : int # total height of the graph
 	var color : Color # color of the graph's line
-	var line_width : float # widht of the graph's line
+	var line_width : float # width of the graph's line
 	
-	func _init(values, pos, central_val = null, val_range = null, total_widht = 560,\
+	func _init(values, pos, central_val = null, val_range = null, total_width = 560,\
 	           total_height = 512, color = Color(1, 1, 1), line_width = 1.0):
 		self.values = values
 		self.pos    = pos
@@ -34,15 +32,17 @@ class LineGraph:
 			self.val_range = max(abs(values.max() - self.central_val),\
 			                     abs(values.min() - self.central_val))
 		
-		self.total_widht  = total_widht
+		self.total_width  = total_width
 		self.total_height = total_height
 		self.color        = color
 		self.line_width   = line_width
 	
 	func graph_points():
-		var current_point = Vector2(self.pos.x, self.pos.y + self.total_height/2)
-		var point_dist    = Vector2(self.total_widht/self.values.size(),\
+		var point_dist    = Vector2(self.total_width/self.values.size(),\
 		                         -self.total_height/(self.values.max() - self.values.min())) # point_dist.y is negative because positive values go down visually
+		var first_diff = self.values[0] - self.central_val
+		print(first_diff)
+		var current_point = Vector2(self.pos.x, self.pos.y + (point_dist.y * first_diff) +self.total_height/2)
 		var points = []
 		points.append(current_point)
 		for i in range(1, self.values.size()):
@@ -50,6 +50,12 @@ class LineGraph:
 			points.append(current_point)
 		
 		return points
+	
+	func get_midpoint():
+		return Vector2(0, self.pos.y + self.total_height/2)
+	
+	func get_central_line_points():
+		return [self.get_midpoint(), self.get_midpoint() + Vector2(self.total_width, 0)]
 
 func _ready():
 	set_process(false)
@@ -58,8 +64,9 @@ func _process(delta):
 	update()
 
 func _draw():
-	var LG = LineGraph.new(values, Vector2(5, 300))
+	var LG = LineGraph.new(values, Vector2(5, 300), 3.0)
 	var points = LG.graph_points()
-	
+	var central = LG.get_central_line_points()
+	draw_line(central[0], central[1], Color(0, 1, 0, .6))
 	for i in range(1, points.size()):
 		draw_line(points[i-1], points[i], Color(1, 1, 1), 2.0, true)
