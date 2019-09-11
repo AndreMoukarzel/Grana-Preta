f64a1da37b72278857fccf74292a95d8396f9e35
Andre Ferrari Moukarzel <Detril@users.noreply.github.com>
2019-09-11 10:38:33 -0300

    Fix ElementDateDefiner not being able to select some values

--- home/organizator/ElementDateDefiner.gd
+++ home/organizator/ElementDateDefiner.gd
@@ -3,14 +3,17 @@ extends Panel
 signal add_element_date(ammount)
 signal no_date()
 
-var ammount : String = "1"
+var ammount : String = "0"
 
 func _ready():
-	$Pad.number = "1"
+	$Confirm.disabled = true
 
 func _on_Pad_value_changed(value):
 	if value == "0":
-		value = "1"
+		$Confirm.disabled = true
+	else:
+		$Confirm.disabled = false
+	
 	if len(value) > 2 and value[0] == "2":
 		value = value[0] + value[-1]
 	if int(value) > 28:
